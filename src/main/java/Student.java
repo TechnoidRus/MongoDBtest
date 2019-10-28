42181d958e3046ba3a33a444024e2cae8a278315
Boris Bovin <b.bovin@mail.ru>
2019-10-28 18:59:24 +0300

    Initial commit

---
+++ src/main/java/Student.java
@@ -0,0 +1,12 @@
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student {
+
+  private String name;
+  private int age;
+  private List<String> courses;
+}
